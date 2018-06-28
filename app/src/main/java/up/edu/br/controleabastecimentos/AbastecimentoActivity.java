d0cd4f910a80d1dfc481774f275ed41259eeb213
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-27 21:05:00 -0300

    Criando Abastecimento Adapter

---
+++ app/src/main/java/up/edu/br/controleabastecimentos/AbastecimentoActivity.java
@@ -0,0 +1,9 @@
+package up.edu.br.controleabastecimentos;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AbastecimentoActivity extends AppCompatActivity{
+
+    Abastecimento abastecimento;
+
+}
---
+++ app/src/main/java/up/edu/br/controleabastecimentos/AbastecimentoAdapter.java
@@ -0,0 +1,52 @@
+package up.edu.br.controleabastecimentos;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AbastecimentoAdapter extends BaseAdapter{
+
+    private List<Abastecimento> abastecimentos;
+    Activity act;
+
+    public AbastecimentoAdapter(List<Abastecimento> abastecimentos, Activity act){
+        this.abastecimentos = abastecimentos;
+        this.act = act;
+    }
+
+    @Override
+    public int getCount() { return this.abastecimentos.size(); }
+
+    @Override
+    public Object getItem(int position) { return this.abastecimentos.get(position); }
+
+    @Override
+    public long getItemId(int position) { return 0; }
+
+    @Override
+    public View getView(int position, View convertVier, ViewGroup parent){
+
+        View v = act.getLayoutInflater().inflate(R.layout.abastecimento_adapter, parent, false);
+
+        TextView txtData = v.findViewById(R.id.txtData);
+        TextView txtOdometro = v.findViewById(R.id.txtOdometro);
+        TextView txtMedia = v.findViewById(R.id.txtMedia);
+        TextView txtLitros = v.findViewById(R.id.txtLitros);
+
+        Abastecimento a = abastecimentos.get(position);
+
+        txtData.setText(a.getData().toString());
+        txtOdometro.setText(a.getOdometro());
+        txtMedia.setText(a.getMedia().toString());
+        txtLitros.setText(a.getLitros().toString());
+
+
+        return v;
+    }
+
+    public void remove (Abastecimento abastecimento) { this.abastecimentos.remove(abastecimento);}
+}
